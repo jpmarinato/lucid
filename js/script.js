62c6fc4b10b3baa4a3373db760df9588bc46d376
jpmarinato <148399011+jpmarinato@users.noreply.github.com>
2024-03-04 17:37:15 -0800

    fixed old version bug, NO NAME, NO IDENTIFY

--- js/script.js
+++ js/script.js
@@ -86,3 +86,31 @@ document.querySelector("#userStats").addEventListener("submit", (event) => {
 
 })
 
+// -------------PANIC BUTTON-------------
+
+function getAway() {
+    // Get away right now
+    window.open("http://weather.com", "_newtab");
+    // Replace current site with another benign site
+    window.location.replace('http://google.com');
+}
+
+$(function() {
+
+    $("#get-away").on("click", function(e) {
+        getAway();
+    });
+
+    $("#get-away a").on("click", function(e) {
+        // allow the (?) link to work
+        e.stopPropagation();
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key
+        getAway();
+        }
+    });
+
+});
+
